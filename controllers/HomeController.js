33296d967ab2f2b34a98d79ae737deff499ab303
hieu26022002 <nguyenngochieu26022002@gmail.com>
2023-05-15 23:05:10 +0700

    first commit

---
+++ controllers/HomeController.js
@@ -0,0 +1,8 @@
+var HomeController = function ($scope) {
+    // $scope(phạm vi) đóng vai trò tương tác qua lại giữa JS và HTML thông qua DOM(Document Object Model)
+    // $scope.name là gán cho phạm vi này 1 cái tên là name có giá trị là Nguyễn Văn A.
+
+    $scope.name = 'Nguyễn Văn A';
+    $scope.id = 'PH12345';
+    $scope.age = 21
+}
---
+++ controllers/ListController.js
@@ -0,0 +1,46 @@
+var ListController = function ($scope, $http, $location) {
+    $scope.title = "Danh sách người dùng";
+    // Mảng 
+    $scope.nguoi_dung = [];
+    $scope.isLoading = true;
+    // Định nghĩa sẵn 1 hàm lấy dữ liệu mới nhất
+    $scope.onGetList = function () {
+        $http.get(API_URL + '/users').then(
+            function (res) {
+                if (res.status == 200) {
+                    $scope.isLoading = false;
+                    $scope.nguoi_dung = res.data;
+                }
+            }, // khi chờ dữ liệu thành công
+            function (err) {
+                console.log(err);
+            } // khi chờ dữ liệu nhưng thất bại
+        );
+    };
+    $scope.onDelete = function (deleteId) {
+        // method delete http://localhost:3000/users/1
+        $http.delete(API_URL + '/users/' + deleteId)
+        .then(
+            function (res) {
+                onGetList();
+            },
+            function (err) {}
+        )
+    };
+
+    // $scope.onEdit = function (editId) {
+    //     // 2 cách
+    //     // 1. Lấy bản ghi trước, sau đó truyền sang màn chỉnh sửa -> Không ổn
+
+    //     // 2. Truyền id qua đường dẫn ở màn chỉnh sửa, rồi mới lấy bản ghi bên đó
+    //     $location.path('/edit/' + editId);
+
+    // };
+
+    // 1. Call API đã định nghĩa ở trang mockAPI.io
+    // $http sẽ có những phương thức như get, post, put... để call API
+    $scope.onGetList();
+
+
+
+}
